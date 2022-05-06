8ece866554eaa9c102eadf7521fbfb904eef2dea
davron <davron.aliyev@lexisnexisrisk.com>
2022-05-06 10:53:05 -0400

    Adding Math class

---
+++ code/Math.java
@@ -0,0 +1,28 @@
+public class Math {
+    public static void main(String[] args) {
+//        addition(2,3);
+//        System.out.println(additionWithReturn(5,5));
+
+        // TC1: (10 < 20) = 0
+        System.out.println(substruction(10, 20));
+        // TC2: (-10, 11) = -21
+        System.out.println(substruction(-10, 11));
+    }
+
+    public static void addition(int num1, int num2){
+        int sum = num1+num2;
+        System.out.println(sum);
+    }
+
+    public static int additionWithReturn(int num1, int num2){
+        int sum = num1+num2;
+        return sum;
+    }
+
+    public static int substruction(int num1, int num2) {
+        if (num1 < num2 && num1 > 0)
+            return 0;
+        else return num1-num2;
+    }
+
+}
