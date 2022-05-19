e5d2e6ff96005855bbf57ec2ab68cb93bede2fca
Davron Aliyev <davron.aliyev@lexisnexisrisk.com>
2022-05-19 09:52:45 -0400

    Adding new Test Case

---
+++ src/test/java/testSuite/TestCases.java
@@ -0,0 +1,27 @@
+package testSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    public void testCase01(){
+        // Open the Browser
+        WebDriver driver = WebDriverManager.chromedriver().create();
+
+        // Navigate to Dashkote website
+        driver.get("https://davracademy.com/");
+
+        // Verify the Title
+        Assert.assertEquals(driver.getTitle(), "Dashkote - Bootstrap5 Admin Template");
+
+        // Close Browser
+        driver.quit();
+    }
+
+
+
+}
